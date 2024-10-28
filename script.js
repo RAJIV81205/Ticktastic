da8a5065aaa0dbe4bd101ff9eb8e3546781576ee
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2024-10-28 22:52:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,52 @@ function hideLoader() {
 
 // Add event listener for the window load event
 window.addEventListener('load', hideLoader);
+
+const currentDate = new Date();
+
+
+document.querySelectorAll(".card").forEach(div =>{
+    div.addEventListener("click", function(){
+        const movieName = this.dataset.movieName;
+        const moviePrice = this.dataset.moviePrice;
+
+        sessionStorage.setItem('movieName', movieName);
+        sessionStorage.setItem('moviePrice', moviePrice);
+        console.log(movieName);
+
+        window.location.href = `booking.html`;
+
+    });
+});
+
+
+function isPaymentPage() {
+    return window.location.pathname.includes('booking.html');
+}
+
+// Run the function only if the current page is payment.html
+window.onload = function () {
+    if (isPaymentPage()) {
+        fetchDetails();
+    }
+}
+
+
+function fetchDetails(){
+    const movieName = sessionStorage.getItem('movieName');
+    const moviePrice = sessionStorage.getItem('moviePrice');
+    const todaydate = currentDate.getDate();
+    const todaymonth = currentDate.getMonth() + 1;
+    const todayyear = currentDate.getFullYear();
+
+
+
+    document.getElementById("movie-name").innerText = ` Movie Name : ${movieName}`;
+    document.getElementById("movie-price").innerText = ` Ticket Price: ${moviePrice}`;
+    document.getElementById("movie-date").innerText = ` Date :${todaydate}/${todaymonth}/${todayyear}`;
+
+}
+
+
+
+
